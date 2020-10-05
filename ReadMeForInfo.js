7d1cd14b23a516ddb26752fdce7e2c26c7361b7a
Jaycen850 <72397234+Jaycen850@users.noreply.github.com>
2020-10-05 16:13:05 +0200

    Update and rename README.md to ReadMeForInfo.js

---
+++ ReadMeForInfo.js
@@ -0,0 +1,4 @@
+# Project_ Thats Me I Learn Coding Today!
+# I Have Discord U to If U Dont Have It Than U Cant Text Me!
+# If U Have Discord My Name Is : LearnCoding#3634
+# Join My Discord Server For Info!
